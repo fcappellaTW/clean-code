41c1208495f915b9303bfc5a809e28361487c77d
Fabrício Cappellari <fabricio.cappellari@thoughtworks.com>
2025-07-21 13:53:42 -0300

    Refactor Movie class

--- src/main/java/com/c/refactoring/movie/Movie.java
+++ src/main/java/com/c/refactoring/movie/Movie.java
@@ -1,8 +1,13 @@
 package com.c.refactoring.movie;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.c.refactoring.StringUtils;
 
 public class Movie {
+
+    private static final List<String> VALID_B_RATINGS = Arrays.asList("1", "2", "3", "4");
 
     String rating;
 
@@ -15,28 +20,34 @@ public class Movie {
         return rating;
     }
 
-    /*Axx or By
-    Where x represents any digit between 0 and 9, and y represents 
-    any digit between 1 and 4*/
-    public boolean isValidRating() {
-        if (this.getRating() != null) {
-            if (this.getRating().substring(0, 1).equalsIgnoreCase("B")
-                    && this.getRating().length() == 2) {
-                if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-                        && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-                        && Integer.parseInt(this.getRating().substring(1, 2)) < 5)
-                    return true;
-
-            } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-                    && this.getRating().length() == 3
-                    && StringUtils.isNumeric(this.getRating().substring(1, 3)))
-                return true;
-
-        }
-        return false;
-    }
-
     public void setRating(String rating) {
         this.rating = rating;
+    }
+
+    public boolean isValidRating() {
+        if (rating == null)
+            return false;
+
+        String gradeType = rating.substring(0, 1);
+        String gradeRating = rating.substring(1);
+
+        switch (gradeType) {
+            case "A":
+                return isGradeARatingValid(gradeRating);
+
+            case "B":
+                return isGradeBRatingValid(gradeRating);
+
+            default:
+                return false;
+        }
+    }
+
+    private boolean isGradeARatingValid(String gradeARating) {
+        return (gradeARating.length() == 2 && StringUtils.isNumeric(gradeARating));
+    }
+
+    private boolean isGradeBRatingValid(String gradeBRating) {
+        return VALID_B_RATINGS.contains(gradeBRating);
     }
 }
