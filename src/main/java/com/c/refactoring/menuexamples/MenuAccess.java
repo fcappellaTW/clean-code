51152f81a598125580b50f5ea8e3f7bdc08e6d1d
Fabrício Cappellari <fabricio.cappellari@thoughtworks.com>
2025-07-21 17:49:04 -0300

    Refactor MenuAccess class

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -1,34 +1,34 @@
 package com.c.refactoring.menuexamples;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuAccess {
 
     public void setAuthorizationsInEachMenus(
             List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
-                }
 
-            }
+        if (roles == null)
+            return;
 
-        }
+        menuItemsList.stream().forEach(menuItem -> setAccessMenuItem(menuItem, roles));
+    }
 
+    private void setAccessMenuItem(MenuItem menuItem, Role[] roles) {
+        boolean hasWriteAccess = validateAccess(roles, menuItem.getWriteAccessRole());
+        boolean hasReadAccess = validateAccess(roles, menuItem.getReadAccessRole());
+
+        if (hasReadAccess)
+            menuItem.setAccess(Constants.READ);
+
+        if (hasWriteAccess)
+            menuItem.setAccess(Constants.WRITE);
+
+        menuItem.setVisible(hasReadAccess || hasWriteAccess);
+    }
+
+    private boolean validateAccess(Role[] roles, String roleName) {
+        return Arrays.stream(roles).anyMatch(role -> role.getName().equals(roleName));
     }
 
 }
